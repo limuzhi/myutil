43040900a6c84f47ad70898ac2338a87e7d63d83
lihui <lihui@channelwill.cn>
2023-01-13 13:21:48 +0800

    初始化

--- aes/aes_test.go
+++ aes/aes_test.go
@@ -1,30 +1,76 @@
 package aes
 
 import (
-	"fmt"
+	"encoding/base64"
 	"testing"
+
+	"github.com/go-pay/gopay/pkg/xlog"
 )
 
-func TestAes(t *testing.T) {
-	secretSrc := "123456"
-	salt := []byte("0okm9ijn8uhb7yg2")
+var (
+	secretKey = "JYRn4wbCy8KgVIZJaPhYTcTn2zixVC4Y"
+	iv        = "JR3unO2glQuMhUx3"
+)
 
-	fmt.Println("加密前:", secretSrc)
-	secretText, err := Encrypt(secretSrc, salt)
+func init() {
+	xlog.Level = xlog.DebugLevel
+}
+
+func TestAesECBEncryptDecrypt(t *testing.T) {
+	originData := "www.gopay.ink"
+	xlog.Debug("originData:", originData)
+	encryptData, err := ECBEncrypt([]byte(originData), []byte(secretKey))
 	if err != nil {
-		t.Error(err)
+		xlog.Error("AesCBCEncryptToString:", err)
 		return
 	}
-	fmt.Println("加密后:", string(secretText))
-
-	secretSrc2, err := Decrypt(secretText, salt)
+	xlog.Debug("encryptData:", string(encryptData))
+	origin, err := ECBDecrypt(encryptData, []byte(secretKey))
 	if err != nil {
-		t.Error(err)
+		xlog.Error("AesDecryptToBytes:", err)
 		return
 	}
-	fmt.Println("解密后:", string(secretSrc2))
+	xlog.Debug("origin:", string(origin))
+}
 
-	if secretSrc != secretSrc2 {
-		t.Error("加密前和解密后值不一致")
+func TestAesCBCEncryptDecrypt(t *testing.T) {
+	originData := "www.gopay.ink"
+	xlog.Debug("originData:", originData)
+	encryptData, err := CBCEncrypt([]byte(originData), []byte(secretKey), []byte(iv))
+	if err != nil {
+		xlog.Error("CBCEncrypt:", err)
+		return
 	}
+	xlog.Debug("encryptData:", string(encryptData))
+	origin, err := CBCDecrypt(encryptData, []byte(secretKey), []byte(iv))
+	if err != nil {
+		xlog.Error("CBCDecrypt:", err)
+		return
+	}
+	xlog.Debug("origin:", string(origin))
+}
+
+func TestEncryptGCM(t *testing.T) {
+	data := `我是要加密的数据`
+	additional := "transaction"
+	apiV3key := "Cj5xC9RXf0GFCKWeD9PyY1ZWLgionbvx"
+	xlog.Debug("原始数据：", data)
+	// 加密
+	nonce, ciphertext, err := GCMEncrypt([]byte(data), []byte(additional), []byte(apiV3key))
+	if err != nil {
+		xlog.Error(err)
+		return
+	}
+	encryptText := base64.StdEncoding.EncodeToString(ciphertext)
+	xlog.Debug("加密后：", encryptText)
+	xlog.Debug("nonce:", string(nonce))
+
+	// 解密
+	cipherBytes, _ := base64.StdEncoding.DecodeString(encryptText)
+	decryptBytes, err := GCMDecrypt(cipherBytes, nonce, []byte(additional), []byte(apiV3key))
+	if err != nil {
+		xlog.Error(err)
+		return
+	}
+	xlog.Debug("解密后：", string(decryptBytes))
 }
